43b6b519051d6e2d68119a3f7cbe3b91aa01240d
karina-henrique-movile <karina.henrique@movile.com>
2014-09-02 10:38:13 -0300

    add project

---
+++ mapkit/mapkit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mapkit
+//
+//  Created by Karina on 02/09/14.
+//  Copyright (c) 2014 Study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ mapkit/mapkit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mapkit
+//
+//  Created by Karina on 02/09/14.
+//  Copyright (c) 2014 Study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
